781931f42729af551f965427928f80c01fc4c318
Md. Sabbir Hossain <82496216+Its-sabbir@users.noreply.github.com>
2024-12-21 14:44:12 +0600

    Update QuickSort.cpp

--- 08. Sorting/QuickSort.cpp
+++ 08. Sorting/QuickSort.cpp
@@ -1,1 +1,36 @@
+#include<iostream>
+#include<vector>
 
+using namespace std;
+
+int partition(vector<int>& arr, int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+    for (int j = low; j < high; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[high]);
+    return i + 1;
+}
+
+void quickSort(vector<int>& arr, int low, int high) {
+
+    if (low >= high) return;
+    int pivot = partition(arr, low, high);
+
+    quickSort(arr, low, pivot - 1);
+    quickSort(arr, pivot + 1, high);
+}
+
+int main() {
+    
+    vector<int> arr = {8,7,0,3,1,2,5,4,9,6};
+    quickSort(arr, 0, arr.size() - 1);
+    for(int x : arr) cout<<x<<" ";
+    cout<<endl;
+
+    return 0;
+}
