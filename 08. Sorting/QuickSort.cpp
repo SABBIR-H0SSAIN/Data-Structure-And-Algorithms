f021c9f2e7102cb738eb7bea3cc97a5aa4ae1b60
Md. Sabbir Hossain <82496216+Its-sabbir@users.noreply.github.com>
2025-01-07 01:10:09 +0600

    Update QuickSort.cpp

--- 08. Sorting/QuickSort.cpp
+++ 08. Sorting/QuickSort.cpp
@@ -1,40 +1,43 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
-int partition(vector<int> &arr, int low, int high) {
-    int pivot = arr[high];
-    int i = low - 1;
-    for (int j = low; j < high; j++) {
-        if (arr[j] < pivot) {
-            i++;
-            swap(arr[i], arr[j]);
-        }
+int partition(vector<int>& arr, int low, int high) {
+
+    int pivot = arr[low];
+    int i = low, j = high;
+    
+    while (i < j) {
+        while (i <= high && arr[i] <= pivot) i++;
+        while (j > low && arr[j] > pivot) j--;
+        if (i < j) swap(arr[i], arr[j]);
     }
-    swap(arr[i + 1], arr[high]);
-    return i + 1;
+    
+    swap(arr[low], arr[j]);
+    return j;
 }
 
-void quickSortR(vector<int> &arr, int low, int high) {
-
+void quickSortR(vector<int>& arr, int low, int high) {
+   
     if (low >= high) return;
-    int pivot = partition(arr, low, high);
-
-    quickSortR(arr, low, pivot - 1);
-    quickSortR(arr, pivot + 1, high);
+    int pivotIndex = partition(arr, low, high);
+    
+    quickSortR(arr, low, pivotIndex - 1);
+    quickSortR(arr, pivotIndex + 1, high);
 }
 
-void quickSort(vector<int> &arr){
-    quickSortR(arr,0,arr.size()-1);
+void quickSort(vector<int>& arr) {
+    quickSortR(arr, 0, arr.size() - 1);
 }
 
 int main() {
-    
-    vector<int> arr = {8,7,0,3,1,2,5,4,9,6};
-    quickSort(arr);
-    for(int x : arr) cout<<x<<" ";
-    cout<<endl;
 
+    vector<int> arr ={4, 1, 3, 2, 0};
+    quickSort(arr);
+    
+    for (int x : arr) cout << x << " ";
+    cout << endl;
+    
     return 0;
 }
