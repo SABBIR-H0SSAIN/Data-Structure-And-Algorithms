477d8d1bb1d8de1e8f5ef43367d8b75fe5a426d1
Md. Sabbir Hossain <82496216+SABBIR-H0SSAIN@users.noreply.github.com>
2025-01-26 18:45:36 +0600

    Create hasCycleUndirectedDFS.cpp

---
+++ 06. Graphs/hasCycleUndirectedDFS.cpp
@@ -0,0 +1,28 @@
+// Practice: https://www.geeksforgeeks.org/problems/detect-cycle-in-an-undirected-graph/1?itm_source=geeksforgeeks&itm_medium=article&itm_campaign=practice_card
+
+class Solution {
+  public:
+    bool dfs(vector<vector<int>>&adj,vector<bool>&vis,int node,int parent){
+        vis[node]=true;
+        
+        for(int child : adj[node]){
+            
+            if(!vis[child]){
+                if(dfs(adj,vis,child,node)) return true;
+            }else if(child !=parent){
+                return true;
+            }
+        }
+        return false;
+    }
+    bool isCycle(vector<vector<int>>& adj) {      
+        vector<bool>vis(adj.size(),false);
+      
+        for(int i=0;i<adj.size();i++){
+            if(vis[i]) continue;
+            if(dfs(adj,vis,i,-1)) return true;
+        }
+        return false;
+    }
+};
+
