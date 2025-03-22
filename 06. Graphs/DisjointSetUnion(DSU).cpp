3ae19a20dd11d9a11330019ff8cdfbd76b629f3f
Md. Sabbir Hossain <82496216+SABBIR-H0SSAIN@users.noreply.github.com>
2025-03-22 15:15:48 +0600

    Update DisjointSetUnion(DSU).cpp

--- 06. Graphs/DisjointSetUnion(DSU).cpp
+++ 06. Graphs/DisjointSetUnion(DSU).cpp
@@ -1,8 +1,4 @@
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
+// @DSU by RANK
 class DSU {
     vector<int> parent, rank;
 
@@ -37,22 +33,64 @@ public:
     }
 };
 
-int main() {
-    DSU dsu(8);
-    dsu.connect(1,3);
-    dsu.connect(2,4);
-    dsu.connect(7,8);
-    dsu.connect(2,3);
+// @DSU by SIZE
+class DSU {
+    vector<int> parent, size;
 
-    if (dsu.isConnected(1, 4)) {
-        cout << "1-4 connected" << endl;
-    } else {
-        cout << "1-4 not connected" << endl;
+public:
+    DSU(int n) {
+        parent.resize(n + 1);
+        size.resize(n + 1, 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+        }
     }
-    if (dsu.isConnected(1, 7)) {
-        cout << "1-7 connected" << endl;
-    } else {
-        cout << "1-7 not connected" << endl;
+
+    int findParent(int child) {
+        if (parent[child] == child) return child;
+        return parent[child] = findParent(parent[child]);
     }
-    return 0;
-}
+
+    bool isConnected(int u, int v) {
+        return findParent(u) == findParent(v);
+    }
+
+    void connect(int u, int v) {
+        u = findParent(u);
+        v = findParent(v);
+
+        if (u != v) {
+            if (size[u] > size[v]) {
+                parent[v] = u;
+                size[u] += size[v];
+            } else {
+                parent[u] = v;
+                size[v] += size[u];
+            }
+        }
+    }
+
+    int getSize(int node) {
+        return size[findParent(node)];
+    }
+};
+
+// int main() {
+//     DSU dsu(8);
+//     dsu.connect(1,3);
+//     dsu.connect(2,4);
+//     dsu.connect(7,8);
+//     dsu.connect(2,3);
+
+//     if (dsu.isConnected(1, 4)) {
+//         cout << "1-4 connected" << endl;
+//     } else {
+//         cout << "1-4 not connected" << endl;
+//     }
+//     if (dsu.isConnected(1, 7)) {
+//         cout << "1-7 connected" << endl;
+//     } else {
+//         cout << "1-7 not connected" << endl;
+//     }
+//     return 0;
+// }
