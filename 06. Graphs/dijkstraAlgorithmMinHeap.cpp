f74aac1d01235102713f56153b86a0f1849d5454
Md. Sabbir Hossain <82496216+SABBIR-H0SSAIN@users.noreply.github.com>
2025-10-10 22:37:10 +0600

    Update dijkstraAlgorithmMinHeap.cpp

--- 06. Graphs/dijkstraAlgorithmMinHeap.cpp
+++ 06. Graphs/dijkstraAlgorithmMinHeap.cpp
@@ -1,35 +1,45 @@
 // Problem Link : https://www.geeksforgeeks.org/problems/implementing-dijkstra-set-1-adjacency-matrix/1
 
+typedef pair<int,int> pii;
+#define inf 1e8
+
 class Solution {
   public:
-    // Function to find the shortest distance of all the vertices
-    // from the source vertex src.
-    vector<int> dijkstra(vector<vector<pair<int, int>>> &adj, int src) {
+    vector<int> dijkstra(int V, vector<vector<int>> &edges, int src) {
+        int v=edges.size();
+        vector<vector<pii>>g(v+1);
+        vector<int>dist(v+1,inf);
         
-        vector<int>dist(adj.size(),INT_MAX);
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>>pq;
-        dist[src]=0;
+        for(auto &e: edges){
+            int u=e[0];
+            int v=e[1];
+            int w=e[2];
+            
+            g[u].push_back({v,w});
+            g[v].push_back({u,w});
+        }
+        
+        priority_queue<pii,vector<pii>,greater<pii>>pq;
         pq.push({0,src});
+        dist[src]=0;
         
         while(!pq.empty()){
-            
-            int node=pq.top().second;
-            int distance=pq.top().first;
+            int node = pq.top().second;
             pq.pop();
             
-            for(auto it : adj[node]){
+            for(auto &adj: g[node]){
+                int adjnode=adj.first;
+                int wt=adj.second;
                 
-                int child =it.first;
-                int wt =it.second;
                 
-                if (distance > dist[node]) continue;
-                
-                if(dist[node]+wt < dist[child]){
-                    dist[child]=dist[node]+wt;
-                    pq.push({dist[child],child});
+                if(dist[node]+wt< dist[adjnode]){
+                    dist[adjnode]=dist[node]+wt;
+                    pq.push({dist[adjnode],adjnode});
                 }
             }
         }
+        
         return dist;
+        
     }
 };
