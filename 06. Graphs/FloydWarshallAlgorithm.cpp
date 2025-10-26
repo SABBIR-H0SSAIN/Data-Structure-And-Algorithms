50816ff9c6f62c0f1b97c7332bb9a96f94e48966
Md. Sabbir Hossain <82496216+SABBIR-H0SSAIN@users.noreply.github.com>
2025-10-26 22:19:30 +0600

    Create FloydWarshallAlgorithm.cpp

---
+++ 06. Graphs/FloydWarshallAlgorithm.cpp
@@ -0,0 +1,17 @@
+//Problem Link: https://www.geeksforgeeks.org/problems/implementing-floyd-warshall2042/1
+
+class Solution {
+  public:
+    void floydWarshall(vector<vector<int>> &dist) {
+        int n=dist.size();
+        for(int k=0;k<n;k++){
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                    if(dist[i][k] ==1e8 || dist[k][j]==1e8) continue;
+                    int d=dist[i][k]+dist[k][j];
+                    dist[i][j]=min(dist[i][j],d);
+                }
+            }
+        }
+    }
+};
